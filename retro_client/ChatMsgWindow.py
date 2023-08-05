64fddc7bde38484db3443a41a290d6e8fc55b2a0
Lukas Wiese <eseiwsakul@gmail.com>
2023-08-05 23:41:05 +0200

    User can selected single messages now instead of just scrolling the line buffer. We need this soon ...

--- retro_client/ChatMsgWindow.py
+++ retro_client/ChatMsgWindow.py
@@ -24,10 +24,13 @@ from . ui.FileDownloadWindow import filesize_to_string
 
 
 Internally the messages are stored as a list of lines.
-There are some special lines, determined by their prefix.
+Each list item is a tuple, where index 0 only is set
+if a line points to the beginning of a message.
 
-All messages start with the following line:
-	/M/M/M<SENDER>/<TIMESTAMP>
+	index 0: message dict (or None)
+	index 1: text (string)
+
+There are some special lines, determined by their prefix.
 
 A file info line:
 	/F/F/F<FILENAME>/<FILESIZE_STRING>/<DOWNLOADED>
@@ -39,9 +42,6 @@ starts with:
 A dimmed line starts with:
 	/D/D/D
 
-
-Date
- Mo, 2. july 20:21
 """
 
 class ChatMsgWindow:
@@ -56,31 +56,30 @@ class ChatMsgWindow:
 		self.friend = None
 
 		# All messages as a list of lines.
-		# Each message has a leading line containing the
-		# sender name and timestamp followed by the message
-		# text:
-		# 	msg1-line0: "/M/M/M[SENDER]/[TIMESTAMP]"
-		# 	msg1-line1: text
-		# 	msg1-lineN: ...
-		# 	msg2-line0: "/U/U/U
-		#	...
-		# All messages are separated by an empty line.
-		#
 		self.lines      = []
 		self.num_msgs   = 0	# Number of messages
 		self.num_unseen = 0	# Number of unseen messages
 
-		# Currently selected line
-		self.cy = 0
+		self.vy = 0	# Index of 1th line shown at screen
+		self.cy = 0	# Index of currently selected line
 
 		# Textwrapper for adjust message body to
 		# window width
 		_,w = self.W.getmaxyx()
 		self.tw = textwrap.TextWrapper(w-4)
-		self.last_color = 0 # For w.attron
 
 		# Screen content has changed?
 		self.changed = True
+
+
+	def get_selected(self):
+		"""\
+		Return the currently selected message (dict)
+		or None on error.
+		"""
+		if self.lines[self.cy][0] is not None:
+			return self.lines[self.cy][0]
+		else:	return None
 
 
 	def add_msg(self, msg):
@@ -98,30 +97,28 @@ class ChatMsgWindow:
 			# were no "unseen" messages yet, add an
 			# "unseen"-marker-line.
 			if self.num_unseen == 0:
-				self.lines.append("/U/U/U")
-				self.lines.append("")
+				self.lines.append((None, "/U/U/U"))
+				self.lines.append((None, ""))
 			self.num_unseen += 1
 
-		# Add message header ("<sender> <msg-time>")
-		self.lines.append("/M/M/M{}/{}".format(
-			msg['from'],
-			self.__format_msgtime(msg['time']) ))
+		# Add message header (dict)
+		self.lines.append((msg, ""))
 
 		if msg['type'] == Proto.T_FILEMSG:
 			# File message
 			# TODO What happens if filename too long?
 			ssize = filesize_to_string(msg['size'])
-			self.lines.append("/F/F/F{}/{}/{}"\
+			self.lines.append((None, "/F/F/F{}/{}/{}"\
 				.format(msg['filename'], ssize,
-					msg['downloaded']))
+					msg['downloaded'])))
 			if not msg['downloaded']:
-				self.lines.append('/D/D/D'\
-					'Press [ctrl+D] to download')
+				self.lines.append((None, '/D/D/D'\
+					'Press [ctrl+D] to download'))
 		else:
 			# Message
 			self.__add_wrap_text(msg['msg'])
 
-		self.lines.append("")
+		self.lines.append((None, ""))
 		self.num_msgs += 1
 
 
@@ -141,28 +138,28 @@ class ChatMsgWindow:
 
 	def reset_view(self):
 		"""\
-		Set view point that latest message can be seen.
+		Set view (self.vy) that latest message can be seen.
 		"""
-		h,_ = self.gui.W['main'].getmaxyx()
-		boxh   = h-3
-		nlines = len(self.lines)
-		self.cy = 0 if nlines<boxh else nlines-boxh-1
-		self.changed = True
+		last_msg_y = self.__last_msg_index()
+		if last_msg_y != None:
+			self.cy = last_msg_y
+		self.__adjust_view()
 
 
 	def scroll_up(self):
 		""" Scroll up """
-		if self.cy > 0:
-			self.cy -= 1
-			self.changed = True
+		y = self.__prev_msg_index()
+		if y != None:
+			self.cy = y
+			self.__adjust_view()
+
 
 	def scroll_down(self):
 		""" Scroll down """
-		h,_ = self.W.getmaxyx()
-
-		if self.cy < len(self.lines)-h+2:
-			self.cy += 1
-			self.changed = True
+		y = self.__next_msg_index()
+		if y != None:
+			self.cy = y
+			self.__adjust_view()
 
 
 	def redraw(self, force_redraw=False):
@@ -183,19 +180,21 @@ class ChatMsgWindow:
 				self.__draw_headline(h, w)
 
 				y = 2
-				for line in self.lines[self.cy:]:
+				for i,line in enumerate(self.lines[self.vy:]):
 					if y >= h-1: break
 
-					if line[:6] == '/M/M/M':
-						self.__print_msg_header(y, line)
-					elif line[:6] == '/F/F/F':
-						self.__print_file_msg(y, line)
-					elif line[:6] == "/D/D/D":
-						self.W.addstr(y, 2, line[6:], curses.A_DIM)
-					elif line == "/U/U/U":
+					if line[0] is not None:
+						# Start of message
+						is_sel = self.vy+i == self.cy
+						self.__print_msg_header(y, line[0], is_sel)
+					elif line[1][:6] == '/F/F/F':
+						self.__print_file_msg(y, line[1])
+					elif line[1][:6] == "/D/D/D":
+						self.W.addstr(y, 2, line[1][6:], curses.A_DIM)
+					elif line[1] == "/U/U/U":
 						self.__print_unseen_marker_line(y, w)
 					else:
-						self.__print_msg(y, line)
+						self.__print_msg(y, line[1])
 					y += 1
 
 				self.__draw_scrollbar(2, w-2, h-3)
@@ -216,13 +215,16 @@ class ChatMsgWindow:
 		"""
 		while True:
 			try:
-				i = self.lines.index("/U/U/U")
+				i = self.lines.index((None,"/U/U/U"))
 				self.lines.pop(i)
 				self.lines.pop(i)
 			except ValueError:
 				break
 
 	def close(self):
+		"""\
+		Clear and refresh chat message window.
+		"""
 		self.gui.winLock.acquire()
 		try:
 			self.W.clear()
@@ -233,46 +235,61 @@ class ChatMsgWindow:
 	#-- PRIVATE ------------------------------------------------------
 
 	def __add_wrap_text(self, msg_text):
-		# Add message text to line list.
+		"""\
+		Add message text to line list.
+		"""
 		lines = msg_text.splitlines()
 		for line in lines:
-			self.lines += self.tw.wrap(line)
-
+#			self.lines += self.tw.wrap(line)
+			for l in self.tw.wrap(line):
+				self.lines.append((None, l))
 
 	def __draw_headline(self, h, w):
-		# Draw the 1th line (headline) of the chat message window.
+		"""\
+		Draw the 1th line (headline) of the chat message window.
+		"""
 		self.W.addstr(1, 1, " "*(w-2), self.gui.colors['Wb'])
 		self.W.addstr(1, 1, " Conversation with " + self.friend.name,
 			self.gui.colors['Wb']|curses.A_BOLD)
-		s = str(self.num_msgs)
-		self.W.addstr(1, w-2-len(s), s, self.gui.colors['Wb'])
+#		s = "h={} vy={} cy={}".format(h, self.vy, self.cy)
+#		s = str(self.num_msgs)
+#		self.W.addstr(1, w-2-len(s)-1, s, self.gui.colors['Wb'])
 
 
-	def __print_msg_header(self, y, line):
-		# Print a message header "SENDER (TIME)"
-		sender,dt = line[6:].split('/')
+	def __print_msg_header(self, y, msg, is_selected):
+		"""\
+		Print a message header "SENDER (TIME)"
+		If is_selected, sender name and time will
+		be shown underlined.
+		"""
+		sender = msg['from']
+		dt = self.__format_msgtime(msg['time'])
 
 		if sender == self.gui.cli.account.name:
 			attr = self.gui.colors['b']
 			sender = 'you'
 		else:	attr = self.gui.colors['g']
 
-		# Print sender name and sent time
-		self.W.addstr(y, 1, sender, attr)
-		self.W.addstr(" ("+dt+")", curses.A_DIM)
+		u = curses.A_UNDERLINE if is_selected else 0
+		self.W.addstr(y, 1, sender, attr|u)
+		self.W.addstr(" ("+dt+")", curses.A_DIM|u)
 
 
 	def __print_unseen_marker_line(self, y, w):
-		# Print a line marking the beginning of unseen
-		# messages.
+		"""\
+		Print a line marking the beginning of unseen
+		messages.
+		"""
 		s = " {} new messages ".format(self.num_unseen)
 		self.W.addstr(y, 1, "-"*(w-2), self.gui.colors['r'])
 		self.W.addstr(y, int(w/2-len(s)/2), s, self.gui.colors['r'])
 
 
 	def __print_file_msg(self, y, line):
-		# Print file message.
-		# /F/F/F<FILENAME>/<FILESIZE_STRING>/<DOWNLOADED>
+		"""\
+		Print file message.
+		/F/F/F<FILENAME>/<FILESIZE_STRING>/<DOWNLOADED>
+		"""
 		fname,fsize,downl = line[6:].split('/')
 		self.W.addstr(y, 2, 'File ')
 		self.W.addstr("'"+fname+"'", curses.A_BOLD)
@@ -320,13 +337,15 @@ class ChatMsgWindow:
 				self.W.addch(ch, attr)
 				i += 1
 
+
 	def __draw_scrollbar(self, y, x, h):
 		"""\
 		Draw scrollbar at the right side of window.
 		"""
-
+				
 		# Get max cursor y position
 		cymax = len(self.lines) - h - 1
+#		cymax = self.num_msgs - h - 1
 		if cymax < 0: cymax = 0
 
 		# Get scrollbar block position
@@ -373,4 +392,86 @@ class ChatMsgWindow:
 
 		# Message is from today
 		return time.strftime("%H:%M", tm)
+
+
+	def __prev_msg_index(self):
+		"""\
+		Returns index of beginning of previous
+		message in self.lines starting at self.cy.
+		If no previous message exists, None is returned.
+		"""
+		y = self.cy-1
+		while y >= 0:
+			if self.lines[y][0]:
+				return y
+			y -= 1
+		return None
+
+	def __next_msg_index(self):
+		"""\
+		Returns index of beginning of next
+		message in self.lines starting at self.cy.
+		If no next message exists, None is returned.
+		"""
+		y = self.cy+1
+		while y < len(self.lines):
+			if self.lines[y][0]:
+				return y
+			y += 1
+		return None
+
+	def __last_msg_index(self):
+		"""\
+		Get index of last message.
+		"""
+		y = len(self.lines)-1
+		while y >= 0:
+			if self.lines[y][0]:
+				return y
+			y -= 1
+		return None
+
+
+	def __selected_msg_nlines(self):
+		"""\
+		Get the number of lines, the currently
+		selected message is using (without headline).
+		"""
+		prefixes = ("/M/M/M","/U/U/U")
+		y = self.cy+1
+		n = 0
+		for line in self.lines[y:]:
+			if line[0] or line[1] in prefixes:
+				return n
+			n += 1
+		return n
+
+	def __adjust_view(self):
+		"""\
+		Adjusts the view (self.vy) that currently
+		selected message (self.cy) is within viewport.
+		"""
+		h,w = self.W.getmaxyx()
+		h -= 3
+
+		if self.cy < self.vy:
+			self.vy = self.cy
+
+		elif self.cy >= self.vy+h:
+			# Set view index, that we can see
+			# the selected message completely.
+			nlines = self.__selected_msg_nlines()
+			self.vy = self.cy-h+nlines+1
+
+		self.changed = True
+"""
+0
+1 _____
+2  |		v0 = 2
+3  |		h  = 4
+4  |		cy = 6
+5 _|___
+6
+
+"""
 
